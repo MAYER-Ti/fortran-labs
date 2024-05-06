b0f1e2afbdf323380bbc7cd73dc4faac2726ee00
MAYER-Ti <s.lutov@inbox.ru>
2024-05-07 01:13:36 +0300

    add ex_2_writting var_5

--- AiSD/labs/var_5/ex_2_writting/src/source_process.f90
+++ AiSD/labs/var_5/ex_2_writting/src/source_process.f90
@@ -6,13 +6,12 @@ module Source_Process
 
 contains
 
-    subroutine MovePartList(indexFirstLine, indexLastLine, indexToPaste, Code)
+    pure subroutine MovePartList(indexFirstLine, indexLastLine, indexToPaste, Code)
        integer, intent(in) :: indexFirstLine, indexLastLine
        integer, intent(inout) :: indexToPaste
        type(SourceLine), pointer, intent(inout) :: Code
 
        type(SourceLine), pointer :: CodeToInsert, TailToInsert
-       TailToInsert => Null()
 
       ! type(SourceLine), pointer :: beforeFirstLine
       ! type(SourceLine), pointer :: lastLine
@@ -20,9 +19,11 @@ contains
       ! type(SourceLine), pointer :: tmp1
       ! type(SourceLine), pointer :: tmp2
 
-       call CutSearchDiap(Code, CodeToInsert, TailToInsert, indexFirstLine, indexLastLine, indexToPaste, 1)
-!       call InsertDiap(Code, CodeToInsert, TailToInsert, indexToPaste, 0)
-      call WriteCode("Output.txt", CodeToInsert, "append", "--------- test ----------")
+       !call MoveToTail(CodeToInsert, TailToInsert, Code)
+       call CutSearchDiap(Code, CodeToInsert, TailToInsert, indexFirstLine, indexLastLine, indexToPaste)
+       !call WriteCode("Output.txt", CodeToInsert, "append", "--------- test ----------")
+       call InsertDiap(Code, CodeToInsert, TailToInsert, indexToPaste)
+       !call WriteCode("Output.txt", Code, "append", "--------- test source ----------")
        
        
 
@@ -48,21 +49,47 @@ contains
 
     end subroutine MovePartList 
 
-    pure recursive subroutine CutSearchDiap(line, CodeToInsert, TailToInsert, indexFirstLine, indexLastLine, indexToPaste, indexCur)
-       type(SourceLine), pointer, intent(inout) :: line, CodeToInsert, TailToInsert
+    pure subroutine CutSearchDiap(SourceCode, CodeToInsert, TailToInsert, indexFirstLine, indexLastLine, indexToPaste)
+       type(SourceLine), pointer, intent(inout) :: SourceCode, CodeToInsert, TailToInsert
        integer, intent(inout) :: indexToPaste
-       integer, intent(in) :: indexFirstLine, indexLastLine, indexCur
+       integer, intent(in) :: indexFirstLine, indexLastLine
 
-       if(indexFirstLine <= indexCur .and. indexCur <= indexLastLine) then
-          ! Переместить line в CodeToInsert
-          call MoveToTail(CodeToInsert, TailToInsert, line)
-          ! Сдвинуть индекс вставки
-          if(indexToPaste > indexLastLine) indexToPaste = indexToPaste - 1
-          ! После перемещения ссылка line уже передвинута
-          call CutSearchDiap(line%next, CodeToInsert, TailToInsert, indexFirstLine, indexLastLine, indexToPaste, indexCur+1) 
-       else  if (Associated(line%next)) then 
-         call CutSearchDiap(line%next,CodeToInsert, TailToInsert, indexFirstLine, indexLastLine, indexToPaste, indexCur+1)  
-       end if
+       integer :: indexCur
+       type(SourceLine), pointer :: curLine
+       type(SourceLine), pointer :: tmpBeforeList, tmpAfterList
+
+       indexCur = 1
+       curLine => SourceCode
+       do while (Associated(curLine))
+          if(indexCur == indexFirstLine-1) then
+             CodeToInsert  => curLine%next
+             tmpBeforeList => curLine
+              
+          else if(indexCur == indexLastLine) then 
+             TailToInsert => curLine
+             tmpAfterList => curLine%next
+             curLine%next => Null()
+             exit
+          end if
+          curLine => curLine%next
+          indexCur = indexCur + 1
+       end do
+
+       tmpBeforeList%next => tmpAfterList%next
+
+       if(indexToPaste > indexLastLine) &
+           indexToPaste = indexToPaste-2 - (indexLastLine+1-indexFirstLine)
+
+      ! if(indexFirstLine <= indexCur .and. indexCur <= indexLastLine) then
+      !    ! Переместить line в CodeToInsert
+      !    call MoveToTail(CodeToInsert, TailToInsert, line)
+      !    ! Сдвинуть индекс вставки
+      !    if(indexToPaste > indexLastLine) indexToPaste = indexToPaste - 1
+      !    ! После перемещения ссылка line уже передвинута
+      !    call CutSearchDiap(line%next, CodeToInsert, TailToInsert, indexFirstLine, indexLastLine, indexToPaste, indexCur+1) 
+      ! else  if (Associated(line%next)) then 
+      !   call CutSearchDiap(line%next,CodeToInsert, TailToInsert, indexFirstLine, indexLastLine, indexToPaste, indexCur+1)  
+      ! end if
 
     end subroutine CutSearchDiap 
 !
@@ -78,49 +105,71 @@ contains
 !          call GetLineOnIndex(line%next, searchIndex, indexCur+1, searchLine) 
 !    end subroutine GetLineOnIndex
 
-    pure subroutine MoveToTail(Head, Tail, Elem)
-       type(SourceLine), pointer  :: Head, Tail, Elem
+!    pure subroutine MoveToTail(Head, Tail, Elem)
+!       type(SourceLine), pointer  :: Head, Tail, Elem
+!
+!       type(SourceLine), pointer :: tmp
+!       
+!       if (.not. Associated(Head)) then
+!          Head => Elem
+!          Tail => Elem
+!       else 
+!          Tail => Elem
+!       end if
+!       
+!      ! if (.not. Associated(Code)) then
+!      !     ! Первый элемент
+!      !     allocate(Code)
+!      !     Code%String = line%String
+!      !     Tail => Code
+!      ! else if (.not. Associated(Tail%next)) then
+!      !     allocate (Tail%next)
+!      !     Tail%next%String = line%string
+!      !     ! Передвинуть хвост
+!      !     Tail => Tail%next
+!      ! end if
+!       ! Убрать элемент из списка 
+!       tmp => Elem
+!       Elem => Elem%next
+!       deallocate(tmp)
+!
+!    end subroutine MoveToTail
 
-      ! type(SourceLine), pointer :: tmp
+    pure subroutine InsertDiap(Code, CodeToInsert, TailToInsert, indexToPaste)
+        type(SourceLine), pointer, intent(inout) :: Code, CodeToInsert, TailToInsert
+        integer, intent(in)                      :: indexToPaste
 
-      if (.not. Associated(Head)) then
-         Head => Elem
-         Tail => Elem
-      else 
-         Tail => Elem
-      end if
-       
-      ! if (.not. Associated(Code)) then
-      !     ! Первый элемент
-      !     allocate(Code)
-      !     Code%String = line%String
-      !     Tail => Code
-      ! else if (.not. Associated(Tail%next)) then
-      !     allocate (Tail%next)
-      !     Tail%next%String = line%string
-      !     ! Передвинуть хвост
-      !     Tail => Tail%next
-      ! end if
-      ! ! Убрать элемент из списка 
-      ! tmp => line
-      ! line => line%next
-      ! deallocate(tmp)
+        integer :: indexCur
+        type(SourceLine), pointer :: curLine, tmp
 
-    end subroutine MoveToTail
-
-    pure recursive subroutine InsertDiap(line, CodeToInsert, TailToInsert, indexToPaste, indexCur)
-        type(SourceLine), pointer, intent(inout) :: line, CodeToInsert, TailToInsert
-        integer, intent(in)                      :: indexToPaste, indexCur
-
-        type(SourceLine), pointer :: tmp
-
-        if(indexCur == indexToPaste) then
-           tmp => line%next
-           line%next => CodeToInsert
+        curLine => Code
+        indexCur = 0
+        if(indexToPaste == 0) then 
+           tmp  => Code
+           Code => CodeToInsert
            TailToInsert%next => tmp
-        else if(Associated(line%next)) then
-            call InsertDiap(line%next, CodeToInsert, TailToInsert, indexToPaste, indexCur+1)
-        end if
+       else
+           do while(Associated(curLine)) 
+              if(indexCur == indexToPaste) then 
+                 tmp => curLine%next
+                 curLine%next => CodeToInsert
+                 TailToInsert%next => tmp
+                 exit
+              end if
+              curLine => curLine%next
+              indexCur = indexCur + 1
+           end do
+        end if 
+
+
+       !   
+       ! if(indexCur == indexToPaste) then
+       !    tmp => line%next
+       !    line%next => CodeToInsert
+       !    TailToInsert%next => tmp
+       ! else if(Associated(line%next)) then
+       !     call InsertDiap(line%next, CodeToInsert, TailToInsert, indexToPaste, indexCur+1)
+       ! end if
 
     end subroutine InsertDiap
 
